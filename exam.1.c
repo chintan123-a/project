ff38a18604d50dd3030c3bb9b601e1ae215d1191
chintan123-a <chintanbhalala13@gmail.com>
2025-06-07 15:29:18 +0530

    Add files via upload

---
+++ exam.1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int a , b, c, d;
+	
+	printf("enter the number a: ");
+	scanf("%d", &a);
+	
+	printf("enter the number b: ");
+	scanf("%d", &b);
+	
+	printf("enter the number c: ");
+	scanf("%d", &c);
+	
+	d = a + c;
+	
+	printf("sum of %d and %d is %d" , d);
+	scanf(d = a + c);
+}
+/*
+input = a = 10
+        b = 20
+        c = 30
+output = d = 40
+*/
